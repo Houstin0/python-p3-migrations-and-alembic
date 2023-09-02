1d326a113f6e4c642413da980518093ea9690d94
Houstin Angwenyi <houstin.angwenyi@student.moringaschool.com>
2023-09-02 21:34:48 +0300

    completed assignment

---
+++ app/migrations/versions/455c74141b76_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 455c74141b76
+Revises: 
+Create Date: 2023-09-02 21:10:14.109319
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '455c74141b76'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
